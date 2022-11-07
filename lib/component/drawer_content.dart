903ff4ddb22f2a39af57266c7a1d64c5d10b9e40
najmulmyself <najmul.myself@gmail.com>
2022-11-07 17:53:48 +0600

    added drawer modified

--- lib/component/drawer_content.dart
+++ lib/component/drawer_content.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:courier_status/apiService/lot_status_view.dart';
-import 'package:courier_status/screens/home_page.dart';
 import 'package:courier_status/screens/shipment.dart';
 import 'package:courier_status/screens/sign_in_page.dart';
 import 'package:flutter/material.dart';
