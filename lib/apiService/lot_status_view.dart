dec1d6a81e19383ca98caefd3dbd6596d473e6d8
najmulmyself <najmul.myself@gmail.com>
2022-11-07 17:44:28 +0600

    added token to update lot

--- lib/apiService/lot_status_view.dart
+++ lib/apiService/lot_status_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'dart:convert';
 import 'package:courier_status/apiService/setToken.dart';
 import 'package:http/http.dart' as http;
@@ -15,7 +17,6 @@ class ApiService {
     print("Get Update Status View Happend${response.statusCode} ");
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      print("Hello World");
       print(data);
     } else {
       print("Error");
